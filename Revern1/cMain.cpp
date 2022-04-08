9ff37bdef7254ac0b2bac467955a4af060793ea4
draconicode <uli@imagin-air.com>
2022-04-08 16:24:09 +0200

    Add project files.

---
+++ Revern1/cMain.cpp
@@ -0,0 +1,11 @@
+#include "cMain.h"
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Revern", wxPoint(1000,700),wxSize(800, 512))
+{
+	this->SetBackgroundColour(wxColour(20, 20, 24, 255));
+}
+
+cMain::~cMain()
+{
+}
+
